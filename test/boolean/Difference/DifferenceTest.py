523a70130b8648e5252a0c7caed93d642589d46a
Paul Water <p.r.water@setbased.nl>
2024-06-29 11:37:32 +0200

    Add test cases for Difference.

---
+++ test/boolean/Difference/DifferenceTest.py
@@ -0,0 +1,32 @@
+from ScadTestCase import ScadTestCase
+from super_scad.boolean.Difference import Difference
+from super_scad.d3.Cylinder import Cylinder
+from super_scad.Scad import Scad
+from super_scad.transformation.Rotate3D import Rotate3D
+from super_scad.Unit import Unit
+
+
+class DifferenceTest(ScadTestCase):
+    """
+    Test cases for Difference.
+    """
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def testDifference(self):
+        """
+        The test case for Difference.
+        """
+        path_actual, path_expected = self.paths()
+
+        scad = Scad(unit=Unit.MM)
+        difference = Difference(children=[Cylinder(height=4, radius=1, center=True, fn=100),
+                                          Rotate3D(angle_x=90,
+                                                   child=Cylinder(height=4, radius=0.9, center=True, fn=100))])
+
+        scad.run_super_scad(difference, path_actual)
+
+        actual = path_actual.read_text()
+        expected = path_expected.read_text()
+        self.assertEqual(expected, actual)
+
+# ----------------------------------------------------------------------------------------------------------------------
