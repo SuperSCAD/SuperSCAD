1b898672bf6736f1a639e0ff147bd0a44173c9aa
Paul Water <p.r.water@setbased.nl>
2024-06-29 09:32:03 +0200

    Add unit tests for minkowski.

---
+++ test/transformation/Minkowski/MinkowskiTest.py
@@ -0,0 +1,48 @@
+from ScadTestCase import ScadTestCase
+from super_scad.d3.Cuboid import Cuboid
+from super_scad.d3.Cylinder import Cylinder
+from super_scad.Scad import Scad
+from super_scad.transformation.Minkowski import Minkowski
+from super_scad.type.Size3 import Size3
+from super_scad.Unit import Unit
+
+
+class MinkowskiTest(ScadTestCase):
+    """
+    test case for minkowski.
+    """
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def testMinkowski(self):
+        """
+        Test case for minkowski without convexity.
+        """
+        path_actual, path_expected = self.paths()
+
+        scad = Scad(unit=Unit.MM)
+        minkowski = Minkowski(children=[Cuboid(size=Size3(10, 10, 1)),
+                                        Cylinder(radius=2.0, height=1.0)])
+
+        scad.run_super_scad(minkowski, path_actual)
+        actual = path_actual.read_text()
+        expected = path_expected.read_text()
+        self.assertEqual(expected, actual)
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def testMinkowskiWithConvexity(self):
+        """
+        Test case for minkowski with convexity.
+        """
+        path_actual, path_expected = self.paths()
+
+        scad = Scad(unit=Unit.MM)
+        minkowski = Minkowski(convexity=10,
+                              children=[Cuboid(size=Size3(10, 10, 1)),
+                                        Cylinder(radius=2.0, height=1.0)])
+
+        scad.run_super_scad(minkowski, path_actual)
+        actual = path_actual.read_text()
+        expected = path_expected.read_text()
+        self.assertEqual(expected, actual)
+
+# ----------------------------------------------------------------------------------------------------------------------
---
+++ test/transformation/Minkowski/testMinkowskiWithConvexity.expected.scad
@@ -0,0 +1,6 @@
+// Unit of length: Unit.MM
+minkowski(convexity = 10)
+{
+   cube(size = [10.0, 10.0, 1.0], center = false);
+   cylinder(h = 1.0, r = 2.0, center = false);
+}
