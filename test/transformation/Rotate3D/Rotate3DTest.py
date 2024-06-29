dda166c0f447c38aa75f5fcbd900afd75a8ab952
Paul Water <p.r.water@setbased.nl>
2024-06-29 10:23:07 +0200

    Add unit tests for Rotate3D.

---
+++ test/transformation/Rotate3D/Rotate3DTest.py
@@ -0,0 +1,82 @@
+import math
+
+from ScadTestCase import ScadTestCase
+from super_scad.boolean.Union import Union
+from super_scad.d3.Cube import Cube
+from super_scad.d3.Cylinder import Cylinder
+from super_scad.Scad import Scad
+from super_scad.transformation.Paint import Paint
+from super_scad.transformation.Rotate3D import Rotate3D
+from super_scad.type.Color import Color
+from super_scad.type.Point3 import Point3
+from super_scad.Unit import Unit
+
+
+class Rotate3DTest(ScadTestCase):
+    """
+    Test case for rotate.
+    """
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def testRotatePoint3(self):
+        """
+        Test case for rotate with Point3.
+        """
+        path_actual, path_expected = self.paths()
+
+        scad = Scad(unit=Unit.MM)
+
+        end_point = Point3(x=10.0, y=10.0, z=10.0)
+        length = end_point.length()
+        b = math.degrees(math.acos(end_point.z / length))
+        c = math.degrees(math.atan2(end_point.y, end_point.x))
+
+        rotate = Rotate3D(angle=Point3(0.0, b, c), child=Cylinder(height=length, radius=0.5))
+        cube = Paint(color=Color(color='gray', alpha=0.5), child=Cube(size=end_point.x))
+
+        self.assertAlmostEqual(rotate.angle_x, 0.0)
+        self.assertAlmostEqual(rotate.angle_y, b)
+        self.assertAlmostEqual(rotate.angle_z, c)
+        self.assertAlmostEqual(rotate.angle.x, 0.0)
+        self.assertAlmostEqual(rotate.angle.y, b)
+        self.assertAlmostEqual(rotate.angle.z, c)
+
+        union = Union(children=[rotate, cube])
+
+        scad.run_super_scad(union, path_actual)
+        actual = path_actual.read_text()
+        expected = path_expected.read_text()
+        self.assertEqual(expected, actual)
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def testRotateAngles(self):
+        """
+        Test case for rotate with angles.
+        """
+        path_actual, path_expected = self.paths()
+
+        scad = Scad(unit=Unit.MM)
+
+        end_point = Point3(x=10.0, y=10.0, z=10.0)
+        length = end_point.length()
+        b = math.degrees(math.acos(end_point.z / length))
+        c = math.degrees(math.atan2(end_point.y, end_point.x))
+
+        rotate = Rotate3D(angle_y=b, angle_z=c, child=Cylinder(height=length, radius=0.5))
+        cube = Paint(color=Color(color='gray', alpha=0.5), child=Cube(size=end_point.x))
+
+        self.assertAlmostEqual(rotate.angle_x, 0.0)
+        self.assertAlmostEqual(rotate.angle_y, b)
+        self.assertAlmostEqual(rotate.angle_z, c)
+        self.assertAlmostEqual(rotate.angle.x, 0.0)
+        self.assertAlmostEqual(rotate.angle.y, b)
+        self.assertAlmostEqual(rotate.angle.z, c)
+
+        union = Union(children=[rotate, cube])
+
+        scad.run_super_scad(union, path_actual)
+        actual = path_actual.read_text()
+        expected = path_expected.read_text()
+        self.assertEqual(expected, actual)
+
+# ----------------------------------------------------------------------------------------------------------------------
---
+++ test/transformation/Rotate3D/testRotateAngles.expected.scad
@@ -0,0 +1,12 @@
+// Unit of length: Unit.MM
+union()
+{
+   rotate(a = [0.0, 54.7356, 45.0])
+   {
+      cylinder(h = 17.3205, r = 0.5, center = false);
+   }
+   color(c = [0.502, 0.502, 0.502, 0.5])
+   {
+      cube(size = 10.0, center = false);
+   }
+}
