99abe3ce7d3d054848f266bdfe2d1dc6655e1ec0
Paul Water <p.r.water@setbased.nl>
2024-06-29 11:34:40 +0200

    Add test cases for Union.

---
+++ test/boolean/Union/UnionTest.py
@@ -0,0 +1,31 @@
+from ScadTestCase import ScadTestCase
+from super_scad.boolean.Union import Union
+from super_scad.d3.Cylinder import Cylinder
+from super_scad.Scad import Scad
+from super_scad.transformation.Rotate3D import Rotate3D
+from super_scad.Unit import Unit
+
+
+class UnionTest(ScadTestCase):
+    """
+    Test cases for Union.
+    """
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def testUnion(self):
+        """
+        The test case for Union.
+        """
+        path_actual, path_expected = self.paths()
+
+        scad = Scad(unit=Unit.MM)
+        union = Union(children=[Cylinder(height=4, radius=1, center=True, fn=100),
+                                Rotate3D(angle_x=90, child=Cylinder(height=4, radius=0.9, center=True, fn=100))])
+
+        scad.run_super_scad(union, path_actual)
+
+        actual = path_actual.read_text()
+        expected = path_expected.read_text()
+        self.assertEqual(expected, actual)
+
+# ----------------------------------------------------------------------------------------------------------------------
