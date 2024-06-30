a1e2b30fafc60f82b021bb95a02b48ad25572cb7
Paul Water <p.r.water@setbased.nl>
2024-06-30 21:49:19 +0200

    Refactor PieSlice2D and PieSlice2D4n.

--- super_scad/d2/PieSlice2D.py
+++ super_scad/d2/PieSlice2D.py
@@ -1,17 +1,9 @@
-import math
-
-from super_scad.boolean.Difference import Difference
-from super_scad.boolean.Intersection import Intersection
-from super_scad.boolean.Union import Union
-from super_scad.Context import Context
 from super_scad.d2.Circle import Circle
-from super_scad.d2.Polygon import Polygon
+from super_scad.d2.private.PrivatePieSlice2D import PrivatePieSlice2D
 from super_scad.ScadObject import ScadObject
-from super_scad.type.Angle import Angle
-from super_scad.type.Point2 import Point2
 
 
-class PieSlice2D(ScadObject):
+class PieSlice2D(PrivatePieSlice2D):
     """
     Class for 2D pie slices.
     """
@@ -41,65 +33,11 @@ class PieSlice2D(ScadObject):
         :param fs: The minimum circumferential length of each fragment.
         :param fn: The fixed number of fragments in 360 degrees. Values of 3 or more override fa and fs.
         """
-        ScadObject.__init__(self, args=locals())
+        PrivatePieSlice2D.__init__(self, args=locals())
 
     # ------------------------------------------------------------------------------------------------------------------
     def _validate_arguments(self) -> None:
         pass
-
-    # ------------------------------------------------------------------------------------------------------------------
-    @property
-    def angle(self) -> float:
-        """
-        Returns the angle of the pie slice.
-        """
-        return Angle.normalize(self.end_angle - self.start_angle)
-
-    # ------------------------------------------------------------------------------------------------------------------
-    @property
-    def start_angle(self) -> float:
-        """
-        Returns the start angle of the pie slice.
-        """
-        if 'angle' in self._args:
-            return Angle.normalize(self._args['angle']) if self._args['angle'] < 0.0 else 0.0
-
-        return Angle.normalize(self._args['start_angle'])
-
-    # ------------------------------------------------------------------------------------------------------------------
-    @property
-    def end_angle(self) -> float:
-        """
-        Returns the end angle of the pie slice.
-        """
-        if 'angle' in self._args:
-            return Angle.normalize(self._args['angle']) if self._args['angle'] > 0.0 else 0.0
-
-        return Angle.normalize(self._args['end_angle'])
-
-    # ------------------------------------------------------------------------------------------------------------------
-    @property
-    def radius(self) -> float:
-        """
-        Returns the outer radius of the pie slice.
-        """
-        return self.uc(self.outer_radius)
-
-    # ------------------------------------------------------------------------------------------------------------------
-    @property
-    def inner_radius(self) -> float:
-        """
-        Returns the inner radius of the pie slice.
-        """
-        return self.uc(self._args.get('inner_radius', 0.0))
-
-    # ------------------------------------------------------------------------------------------------------------------
-    @property
-    def outer_radius(self) -> float:
-        """
-        Returns the outer radius of the pie slice.
-        """
-        return self.uc(self._args.get('outer_radius', self._args.get('radius', 0.0)))
 
     # ------------------------------------------------------------------------------------------------------------------
     @property
@@ -126,91 +64,12 @@ class PieSlice2D(ScadObject):
         return self._args.get('fn')
 
     # ------------------------------------------------------------------------------------------------------------------
-    @staticmethod
-    def __angular_to_vector(length: float, angle: float, ):
-        return Point2(length * math.cos(math.radians(angle)), length * math.sin(math.radians(angle)))
-
-    # ------------------------------------------------------------------------------------------------------------------
-    def build(self, context: Context) -> ScadObject:
+    def _create_circle(self, radius: float) -> ScadObject:
         """
-        Builds a SuperSCAD object.
+        Creates a circle with given radius.
 
-        :param context: The build context.
+        :param radius: Radius of the circle.
         """
-        angle = self.angle
-        start_angle = self.start_angle
-        end_angle = self.end_angle
-
-        if self.outer_radius <= 0.0 or angle == 0.0:  # xxx Use rounding in target units.
-            return Union(children=[])
-
-        if self.inner_radius == 0.0:  # xxx Use rounding in target units.
-            circles = Circle(radius=self.outer_radius, fa=self.fa, fs=self.fs, fn=self.fn)
-        else:
-            circles = Difference(children=[Circle(radius=self.outer_radius, fa=self.fa, fs=self.fs, fn=self.fn),
-                                           Circle(radius=self.inner_radius, fa=self.fa, fs=self.fs, fn=self.fn)])
-
-        if round(angle - 360.0, 4) == 0.0:  # xxx Use rounding in target units.
-            return circles
-
-        if round(angle - 90.0, 4) < 0.0:  # xxx Use rounding in target units.
-            size2 = self.outer_radius / math.cos(math.radians(Angle.normalize(angle, 90.0) / 2.0)) + context.eps
-            points = [Point2(0.0, 0.0),
-                      self.__angular_to_vector(size2, start_angle),
-                      self.__angular_to_vector(size2, end_angle)]
-
-        elif round(angle - 90.0, 4) == 0.0:  # xxx Use rounding in target units.
-            size1 = math.sqrt(2.0) * self.outer_radius + context.eps
-            size2 = self.outer_radius + context.eps
-            points = [Point2(0.0, 0.0),
-                      self.__angular_to_vector(size2, start_angle),
-                      self.__angular_to_vector(size1, start_angle + 45.0),
-                      self.__angular_to_vector(size2, end_angle)]
-
-        elif round(angle - 180.0, 4) == 0.0:  # xxx Use rounding in target units.
-            size1 = math.sqrt(2.0) * self.outer_radius + context.eps
-            size2 = self.outer_radius + context.eps
-            points = [self.__angular_to_vector(size2, start_angle),
-                      self.__angular_to_vector(size1, start_angle + 45.0),
-                      self.__angular_to_vector(size1, start_angle + 135.0),
-                      self.__angular_to_vector(size2, end_angle)]
-
-        elif round(angle - 270.0, 4) == 0.0:  # xxx Use rounding in target units.
-            size1 = math.sqrt(2.0) * self.outer_radius + context.eps
-            size2 = self.outer_radius + context.eps
-            points = [Point2(0.0, 0.0),
-                      self.__angular_to_vector(size2, start_angle),
-                      self.__angular_to_vector(size1, start_angle + 45.0),
-                      self.__angular_to_vector(size1, start_angle + 135.0),
-                      self.__angular_to_vector(size1, start_angle + 225.0),
-                      self.__angular_to_vector(size2, end_angle)]
-
-        elif round(angle - 180.0, 4) < 0.0:  # xxx Use rounding in target units.
-            phi = Angle.normalize((start_angle - end_angle) / 2.0, 90.0)
-            size1 = math.sqrt(2.0) * self.outer_radius + context.eps
-            size2 = math.sqrt(2.0) * self.outer_radius / (
-                    math.cos(math.radians(phi)) + math.sin(math.radians(phi))) + context.eps
-            points = [Point2(0.0, 0.0),
-                      self.__angular_to_vector(size2, start_angle),
-                      self.__angular_to_vector(size1, start_angle - phi + 90.0),
-                      self.__angular_to_vector(size1, start_angle - phi + 180.0),
-                      self.__angular_to_vector(size2, end_angle)]
-
-        elif round(angle - 360.0, 4) < 0.0:  # xxx Use rounding in target units.
-            phi = Angle.normalize((start_angle - end_angle) / 2.0, 90.0)
-            size1 = math.sqrt(2.0) * self.outer_radius + context.eps
-            size2 = math.sqrt(2.0) * self.outer_radius / (
-                    math.cos(math.radians(phi)) + math.sin(math.radians(phi))) + context.eps
-            points = [Point2(0.0, 0.0),
-                      self.__angular_to_vector(size2, start_angle),
-                      self.__angular_to_vector(size1, start_angle - phi + 90.0),
-                      self.__angular_to_vector(size1, start_angle - phi + 180.0),
-                      self.__angular_to_vector(size1, start_angle - phi + 270.0),
-                      self.__angular_to_vector(size2, end_angle)]
-
-        else:
-            raise ValueError('Math is broken!')
-
-        return Intersection(children=[circles, Polygon(points=points, convexity=2)])
+        return Circle(radius=radius, fa=self.fa, fs=self.fs, fn=self.fn)
 
 # ----------------------------------------------------------------------------------------------------------------------
--- super_scad/d2/PieSlice2D4n.py
+++ super_scad/d2/PieSlice2D4n.py
@@ -1,15 +1,9 @@
-from super_scad.boolean.Difference import Difference
-from super_scad.boolean.Intersection import Intersection
-from super_scad.boolean.Union import Union
-from super_scad.Context import Context
 from super_scad.d2.Circle4n import Circle4n
-from super_scad.d2.Square import Square
+from super_scad.d2.private.PrivatePieSlice2D import PrivatePieSlice2D
 from super_scad.ScadObject import ScadObject
-from super_scad.transformation.Rotate2D import Rotate2D
-from super_scad.transformation.Translate2D import Translate2D
 
 
-class PieSlice2D4n(ScadObject):
+class PieSlice2D4n(PrivatePieSlice2D):
     """
     Class for 2D pie slices.
     """
@@ -33,89 +27,19 @@ class PieSlice2D4n(ScadObject):
         :param inner_radius:
         :param outer_radius:
         """
-        ScadObject.__init__(self, args=locals())
+        PrivatePieSlice2D.__init__(self, args=locals())
 
     # ------------------------------------------------------------------------------------------------------------------
     def _validate_arguments(self) -> None:
         pass
 
     # ------------------------------------------------------------------------------------------------------------------
-    @property
-    def angle(self) -> float:
+    def _create_circle(self, radius: float) -> ScadObject:
         """
-        Returns the angle of the pie slice.
-        """
-        return self.end_angle - self.start_angle
+        Creates a circle with given radius.
 
-    # ------------------------------------------------------------------------------------------------------------------
-    @property
-    def start_angle(self) -> float:
+        :param radius: Radius of the circle.
         """
-        Returns the start angle of the pie slice.
-        """
-        return self._args.get('start_angle', 0.0)
-
-    # ------------------------------------------------------------------------------------------------------------------
-    @property
-    def end_angle(self) -> float:
-        """
-        Returns the end angle of the pie slice.
-        """
-        return self._args.get('end_angle', self._args.get('angle', 0.0))
-
-    # ------------------------------------------------------------------------------------------------------------------
-    @property
-    def radius(self) -> float:
-        """
-        Returns the outer radius of the pie slice.
-        """
-        return self.uc(self.outer_radius)
-
-    # ------------------------------------------------------------------------------------------------------------------
-    @property
-    def inner_radius(self) -> float:
-        """
-        Returns the inner radius of the pie slice.
-        """
-        return self.uc(self._args.get('inner_radius', 0.0))
-
-    # ------------------------------------------------------------------------------------------------------------------
-    @property
-    def outer_radius(self) -> float:
-        """
-        Returns the outer radius of the pie slice.
-        """
-        return self.uc(self._args.get('outer_radius', self._args.get('radius', 0.0)))
-
-    # ------------------------------------------------------------------------------------------------------------------
-    def build(self, context: Context) -> ScadObject:
-        """
-        Builds a SuperSCAD object.
-
-        :param context: The build context.
-        """
-        size = self.outer_radius + 2 * context.eps
-        trans = self.outer_radius + context.eps
-        angle = self.angle
-
-        if angle <= 90.0:
-            return Intersection(children=[
-                Difference(children=[Circle4n(radius=self.outer_radius), Circle4n(radius=self.inner_radius)]),
-                Rotate2D(angle=self.end_angle - 90, child=Square(size=size)),
-                Rotate2D(angle=self.start_angle, child=Square(size=size))])
-
-        children = []
-        if self.start_angle < 0.0 and self.end_angle > 90.0:
-            children.append(Square(size=size))
-        if self.start_angle < 90.0 and self.end_angle > 180.0:
-            children.append(Translate2D(x=-trans, child=Square(size=size)))
-        if self.start_angle < 180.0 and self.end_angle > 270.0:
-            children.append(Translate2D(x=-trans, y=-trans, child=Square(size=size)))
-        children.append(Rotate2D(angle=self.end_angle - 90, child=Square(size=size)))
-        children.append(Rotate2D(angle=self.start_angle, child=Square(size=size)))
-
-        return Intersection(children=[
-            Difference(children=[Circle4n(radius=self.outer_radius), Circle4n(radius=self.inner_radius)]),
-            Union(children=children)])
+        return Circle4n(radius=radius)
 
 # ----------------------------------------------------------------------------------------------------------------------
