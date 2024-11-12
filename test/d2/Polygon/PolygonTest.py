84762c531318a5a4c708428a2e2bc542cda432b8
Paul Water <p.r.water@setbased.nl>
2024-11-12 18:59:23 +0100

    Fix unit test.

--- test/d2/Polygon/PolygonTest.py
+++ test/d2/Polygon/PolygonTest.py
@@ -502,13 +502,12 @@ class PolygonTestCase(ScadTestCase):
         """
         Test polygon with nodes to close to reliable computation of the separation between line segments and nodes.
         """
-        context = Context()
-        scad = Scad(context=Context(delta=2.0))
+        context = Context(delta=1.0)
 
         points = [Vector2(0.0, 0.0), Vector2(1.0, 0.0), Vector2(1.0, 1.0), Vector2(0.0, 1.0)]
         polygon1 = Polygon(points=points)
 
-        self.assertRaises(ValueError, lambda: Color(polygon1.is_clockwise(context)))
+        self.assertRaises(ValueError, lambda: polygon1.is_clockwise(context))
 
     # ------------------------------------------------------------------------------------------------------------------
     def test_extend_by_eps_one_side1(self):
