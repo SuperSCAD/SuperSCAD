561138258192d4c3445ed112774ced598c9e3c61
Paul Water <p.r.water@setbased.nl>
2025-05-17 07:59:37 +0200

    Add method validate_count() to ArgumentValidator.

--- super_scad/scad/ArgumentValidator.py
+++ super_scad/scad/ArgumentValidator.py
@@ -19,7 +19,7 @@ class ArgumentValidator:
 
     # ------------------------------------------------------------------------------------------------------------------
     @staticmethod
-    def _to_string(sets: List[Set[str]]) -> str:
+    def _to_string(sets: List[Set[str] | str]) -> str:
         """
 
         """
@@ -35,35 +35,67 @@ class ArgumentValidator:
         ret = ''
         for i in range(len(sets) - 1):
             ret += str(sets[i]) + ', '
-        ret += ', and ' + str(sets[-1])
+        ret += 'and ' + str(sets[-1])
 
         return ret
 
     # ------------------------------------------------------------------------------------------------------------------
-    def validate_exclusive(self, *args: Set[str]) -> None:
+    def validate_exclusive(self, *args: Set[str] | str) -> None:
         """
         Validates that only arguments belonging to one of the given sets are passed to the constructor of the SuperScad
         widget.
         """
         supplied_sets = []
-        for arg in args:
-            supplied = set.intersection(self.__arguments_set, arg)
+        for index, arg in enumerate(args):
+            if isinstance(arg, str):
+                supplied = arg in self.__arguments_set
+            elif isinstance(arg, set):
+                supplied = set.intersection(self.__arguments_set, arg)
+            else:
+                raise TypeError(f'Argument {index + 1} must be a set or str, got a {type(arg)} instead.')
             if supplied:
                 supplied_sets.append(supplied)
 
         if len(supplied_sets) > 1:
             sets = self._to_string(supplied_sets)
-            raise ValueError('The following set of arguments are not exclusive: {}'.format(sets))
+            raise ValueError(f'The following set of arguments are not exclusive: {sets}.')
 
     # ------------------------------------------------------------------------------------------------------------------
-    def validate_required(self, *args: Set[str]) -> None:
+    def validate_required(self, *args: Set[str] | str) -> None:
         """
         Validates that at least one argument belonging to each of the given sets are passed to the constructor of the
         SuperSCAD widget.
         """
-        for arg in args:
-            supplied = set.intersection(self.__arguments_set, arg)
-            if not supplied:
-                raise ValueError('At least one of these arguments {} must be supplied'.format(str(arg)))
+        for index, arg in enumerate(args):
+            if isinstance(arg, str):
+                supplied = arg in self.__arguments_set
+                if not supplied:
+                    raise ValueError(f'Argument {arg} is mandatory.')
+            elif isinstance(arg, set):
+                supplied = set.intersection(self.__arguments_set, arg)
+                if not supplied:
+                    raise ValueError(f'At least one of these arguments {str(arg)} must be supplied.')
+            else:
+                raise TypeError(f'Argument {index + 1} must be a set or str, got a {type(arg)} instead.')
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def validate_count(self, count: int, *args: Set[str] | str) -> None:
+        """
+        Validates that exactly the given number of arguments are passed to the constructor of the SuperSCAD widget.
+        """
+        actual = 0
+        for index, arg in enumerate(args):
+            if isinstance(arg, str):
+                if arg in self.__arguments_set:
+                    actual += 1
+            elif isinstance(arg, set):
+                if set.intersection(self.__arguments_set, arg):
+                    actual += 1
+            else:
+                raise TypeError(f'Argument {index + 1} must be a set or str, got a {type(arg)} instead.')
+
+        if actual != count:
+            arguments = self._to_string(list(args))
+            raise ValueError(f'Of arguments {arguments} exactly {count} must be supplied, got {actual} instead.')
 
 # ----------------------------------------------------------------------------------------------------------------------
--- test/scad/ArgumentValidatorTest.py
+++ test/scad/ArgumentValidatorTest.py
@@ -19,7 +19,7 @@ class ArgumentValidatorTest(unittest.TestCase):
         self.assertTrue(True)
 
     # ------------------------------------------------------------------------------------------------------------------
-    def test_exclusive_not_exclusive(self):
+    def test_exclusive_not_exclusive_set(self):
         """
         Test exclusive arguments that are not exclusive.
         """
@@ -28,7 +28,25 @@ class ArgumentValidatorTest(unittest.TestCase):
         self.assertRaises(ValueError, lambda: validator.validate_exclusive({'radius'}, {'diameter'}))
 
     # ------------------------------------------------------------------------------------------------------------------
-    def test_required_required(self):
+    def test_exclusive_not_exclusive_str(self):
+        """
+        Test exclusive arguments that are not exclusive.
+        """
+        args = {'radius': 1.0, 'diameter': 0.5}
+        validator = ArgumentValidator(args)
+        self.assertRaises(ValueError, lambda: validator.validate_exclusive('radius', 'diameter'))
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def test_exclusive_not_a_set_or_str(self):
+        """
+        Test invalid arguments.
+        """
+        args = {'radius': 1.0}
+        validator = ArgumentValidator(args)
+        self.assertRaises(TypeError, lambda: validator.validate_exclusive({'radius'}, {'diameter'}, ['height']))
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def test_required_required_sets(self):
         """
         Test required arguments that are given.
         """
@@ -38,12 +56,126 @@ class ArgumentValidatorTest(unittest.TestCase):
         self.assertTrue(True)
 
     # ------------------------------------------------------------------------------------------------------------------
-    def test_required_not_given(self):
+    def test_required_required_sets_and_str(self):
+        """
+        Test required arguments that are given.
+        """
+        args = {'height': 10.0, 'radius': 1.0}
+        validator = ArgumentValidator(args)
+        validator.validate_required('height', {'radius', 'diameter'})
+        self.assertTrue(True)
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def test_required_not_given_sets(self):
         """
         Test required arguments that are given.
         """
         args = {'height': 10.0}
         validator = ArgumentValidator(args)
         self.assertRaises(ValueError, lambda: validator.validate_required({'height'}, {'radius', 'diameter'}))
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def test_required_not_given_sets_and_str1(self):
+        """
+        Test required arguments that are given.
+        """
+        args = {'height': 10.0}
+        validator = ArgumentValidator(args)
+        self.assertRaises(ValueError, lambda: validator.validate_required('height', {'radius', 'diameter'}))
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def test_required_not_given_sets_and_str2(self):
+        """
+        Test required arguments that are given.
+        """
+        args = {'radius': 10.0}
+        validator = ArgumentValidator(args)
+        self.assertRaises(ValueError, lambda: validator.validate_required('height', {'radius', 'diameter'}))
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def test_required_not_a_set_or_str(self):
+        """
+        Test invalid arguments.
+        """
+        args = {'height': 10.0}
+        validator = ArgumentValidator(args)
+        self.assertRaises(TypeError, lambda: validator.validate_required('height', ['radius', 'diameter']))
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def test_argument_count_ok(self):
+        """
+        Test argument count with correct nummber of arguments.
+        """
+        args = {'countersink_radius': 10.0, 'countersink_angle': 90, 'countersink_height': None}
+        validator = ArgumentValidator(args)
+        validator.validate_count(2,
+                                 {'countersink_radius', 'countersink_diameter'},
+                                 'countersink_angle',
+                                 'countersink_height')
+        self.assertTrue(True)
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def test_argument_count_ok_with_double(self):
+        """
+        Test argument count with correct nummber of arguments.
+        """
+        args = {'countersink_radius':   10.0,
+                'countersink_diameter': 21.0,
+                'countersink_angle':    90,
+                'countersink_height':   None}
+        validator = ArgumentValidator(args)
+        validator.validate_count(2,
+                                 {'countersink_radius', 'countersink_diameter'},
+                                 'countersink_angle',
+                                 'countersink_height')
+        self.assertTrue(True)
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def test_argument_count_to_less1(self):
+        """
+        Test argument count with correct nummber of arguments.
+        """
+        args = {'countersink_radius': 10.0, 'countersink_angle': None, 'countersink_height': None}
+        validator = ArgumentValidator(args)
+        self.assertRaises(ValueError, lambda: validator.validate_count(2,
+                                                                       {'countersink_radius', 'countersink_diameter'},
+                                                                       'countersink_angle',
+                                                                       'countersink_height'))
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def test_argument_count_to_less2(self):
+        """
+        Test argument count with correct nummber of arguments.
+        """
+        args = {'countersink_radius': None, 'countersink_angle': None, 'countersink_height': None}
+        validator = ArgumentValidator(args)
+        self.assertRaises(ValueError, lambda: validator.validate_count(2,
+                                                                       {'countersink_radius', 'countersink_diameter'},
+                                                                       'countersink_angle',
+                                                                       'countersink_height'))
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def test_argument_count_to_much(self):
+        """
+        Test argument count with correct nummber of arguments.
+        """
+        args = {'countersink_radius': 10.0, 'countersink_angle': 90.0, 'countersink_height': 10.0}
+        validator = ArgumentValidator(args)
+        self.assertRaises(ValueError, lambda: validator.validate_count(2,
+                                                                       {'countersink_radius', 'countersink_diameter'},
+                                                                       'countersink_angle',
+                                                                       'countersink_height'))
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def test_argument_count_wrong_type1(self):
+        """
+        Test argument count with correct nummber of arguments.
+        """
+        args = {'countersink_radius': 10.0, 'countersink_angle': 90.0, 'countersink_height': None}
+        validator = ArgumentValidator(args)
+        self.assertRaises(TypeError, lambda: validator.validate_count(2,
+                                                                      ['countersink_radius', 'countersink_diameter'],
+                                                                      'countersink_angle',
+                                                                      'countersink_height'))
 
 # ----------------------------------------------------------------------------------------------------------------------
